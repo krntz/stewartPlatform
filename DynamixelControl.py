1aee4c1d7bb15c1aea7e3615a67803a890b4be14
Amandus Krantz <krantz.amandus@protonmail.com>
2022-10-13 14:39:04 +0200

    added support for moving servos based on angles

--- DynamixelControl.py
+++ DynamixelControl.py
@@ -15,7 +15,7 @@ class DynamixelControl:
         self.ADDR_MX_TORQUE_ENABLE          = 24
         self.ADDR_MX_GOAL_POSITION          = 30
         self.ADDR_MX_PRESENT_POSITION       = 36
-        self.ADDR_DXL_MOVING_STATYS         = 46
+        self.ADDR_MX_MOVING_STATUS         = 46
         
         # Data Byte Length
         self.LEN_MX_GOAL_POSITION           = 4
@@ -26,7 +26,7 @@ class DynamixelControl:
         self.TORQUE_DISABLE                 = 0                 # Value for disabling the torque
         self.DXL_MINIMUM_POSITION_VALUE     = 204
         self.DXL_MAXIMUM_POSITION_VALUE     = 820
-        self.DXL_MOVING_STATUS_THRESHOLD    = 20                # Dynamixel moving status threshold
+        self.DXL_DEGREES_PER_STEP           = 0.29              # Value for converting between servo steps and degrees
 
         # Initialize PortHandler instance
         self.portHandler = PortHandler(deviceName)
@@ -66,6 +66,8 @@ class DynamixelControl:
                     print("Dynamixel#%d has been successfully connected" % i)
 
     def __del__(self):
+        self.reset_servos()
+
         # Disable Torque
         for i in self.DXL_ids:
             dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(self.portHandler, i, self.ADDR_MX_TORQUE_ENABLE, self.TORQUE_DISABLE)
@@ -76,6 +78,22 @@ class DynamixelControl:
 
         # Close port
         self.portHandler.closePort()
+
+    def reset_servos(self):
+        """Sets servos back to 0 degrees rotation
+        """
+
+        if self.VERBOSE:
+            print("Reseting servos to start position")
+
+        gp = [{"id" : i, "position" : 512} for i in self.DXL_ids]
+
+        self.sync_write(gp)
+
+        while 1:
+            moving = self.get_moving_status()
+            if not all(moving):
+                break
 
     def sync_write(self, goal_positions):
         """ Writes a goal position to multiple Dynamixel servos
@@ -93,8 +111,11 @@ class DynamixelControl:
         Exception
             If adding positions to groupSyncWrite fails
         """
-    
-    
+        # TODO: make this not hardcoded to odd and even servos
+        for i,j in enumerate(goal_positions):
+            if j["id"] % 2 != 0:
+                goal_positions[i]["position"] = 1024 - j["position"]
+
         # Initialize GroupSyncWrite instance
         groupSyncWrite = GroupSyncWrite(self.portHandler, self.packetHandler, self.ADDR_MX_GOAL_POSITION, self.LEN_MX_GOAL_POSITION)
     
@@ -103,13 +124,14 @@ class DynamixelControl:
             if not (i["position"] <= self.DXL_MAXIMUM_POSITION_VALUE and i["position"] >= self.DXL_MINIMUM_POSITION_VALUE):
                 raise ValueError("Goal position %d may cause damage to platform" % i["position"])
 
+
             param_goal_position = [DXL_LOBYTE(DXL_LOWORD(i["position"])),
                                    DXL_HIBYTE(DXL_LOWORD(i["position"])),
                                    DXL_LOBYTE(DXL_HIWORD(i["position"])),
                                    DXL_HIBYTE(DXL_HIWORD(i["position"]))]
             dxl_addparam_result = groupSyncWrite.addParam(i["id"], param_goal_position)
             if self.VERBOSE:
-                print("Sending %d to Dynamixel Servo #%d" % (gp, i["id"]))
+                print("Sending %d to Dynamixel Servo #%d" % (i["position"], i["id"]))
             if dxl_addparam_result != True:
                 raise Exception("[ID:%03d] groupSyncWrite addparam failed" % i["id"])
     
@@ -121,14 +143,54 @@ class DynamixelControl:
         # Clear syncwrite parameter storage
         groupSyncWrite.clearParam()
 
-    def get_moving_status(self, servoID):
-        # Wait until servos have stopped moving
+    def get_current_positions(self):
+        """Queries connected servos for their current positions.
 
-        dxl_moving_status, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, servoID, 46)
+        Returns
+        -------
+        current_positions : list of dicts
+            Ids of servos and their respective current positions
+        """
+        current_positions = []
 
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+        for i in self.DXL_ids:
+            dxl_current_position, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, i, self.ADDR_MX_PRESENT_POSITION)
 
-        return dxl_moving_status
+            if dxl_comm_result != COMM_SUCCESS:
+                print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
+            current_positions.append({"id": i, "position" : dxl_current_position})
+
+        return current_positions
+
+    def move_degrees(self, degrees):
+        """Moves connected servos by indicated number of degrees.
+
+        Parameters
+        ----------
+        degrees : list of dicts, mandatory
+            Ids of servos and the degrees to rotate them
+        """
+
+        current_positions = self.get_current_positions()
+
+        goal_positions = [{"id" : j["id"], "position" : int(j["degrees"] / 0.30) + current_positions[i]["position"]} for i,j in enumerate(degrees)]
+        
+        self.sync_write(goal_positions)
+
+    def get_moving_status(self):
+        moving_status = []
+
+        for i in self.DXL_ids:
+            dxl_moving_status, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, i, self.ADDR_MX_MOVING_STATUS)
+
+            if dxl_comm_result != COMM_SUCCESS:
+                print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+            elif dxl_error != 0:
+                print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
+            moving_status.append(dxl_moving_status)
+
+        return moving_status
