306b5d0121665cc6fdcfdb153828bd834fd9e697
Amandus Krantz <krantz.amandus@protonmail.com>
2022-11-14 22:17:46 +0100

    keyboard control makes a little bit more sense now

--- stewartPlatformControl.py
+++ stewartPlatformControl.py
@@ -16,13 +16,18 @@ def update_servos(new_trans, new_rot):
     else:
         trans = new_trans
         rot = new_rot
-    print(trans, rot)
     return trans, rot
 
-s = StewartPlatform(100.089, 50, 26.5, 120, 0.229, 0.229, np.radians(60), verbose=False)
+s = StewartPlatform(radiusBase = 100.089, 
+                    radiusPlatform = 50, 
+                    servoHornLength = 26.5, 
+                    rodLength = 120, 
+                    anchorAngleBase = 0.229, 
+                    anchorAnglePlatform = 0.229, 
+                    platform_rot = np.radians(60), 
+                    verbose=False)
 
-servo_ids = [2, 3, 4, 5, 6, 7]
-dc = DynamixelControl(servo_ids)
+dc = DynamixelControl([2, 3, 4, 5, 6, 7])
 
 trans = np.array([0, 0, 0])
 rot = np.array([0, 0, 0])
@@ -30,54 +35,46 @@ rot = np.array([0, 0, 0])
 # move servos to home position
 update_servos(trans, rot)
 
-move_speed = 10
+move_speed = 20
+
+# TODO:
+# - Make this a more robust input handler, maybe something from a game engine?
 
 with KeyPoller.KeyPoller() as keyPoller:
     while True:
-        old_trans = trans
-        old_rot = rot
         c = keyPoller.poll()
-        try:
-            if not c is None:
+        if not c is None:
                 if c == "c":
                     break
                 if c == "w":
-                    new_trans = old_trans + np.array([move_speed, 0, 0])
-                    trans, rot = update_servos(new_trans, old_rot)
+                    trans += np.array([move_speed, 0, 0])
                 if c == "a":
-                    new_trans = old_trans - np.array([0, move_speed, 0])
-                    trans, rot = update_servos(new_trans, old_rot)
+                    trans -= np.array([0, move_speed, 0])
                 if c == "s":
-                    new_trans = old_trans - np.array([move_speed, 0, 0])
-                    trans, rot = update_servos(new_trans, old_rot)
+                    trans -= np.array([move_speed, 0, 0])
                 if c == "d":
-                    new_trans = old_trans + np.array([0, move_speed, 0])
-                    trans, rot = update_servos(new_trans, old_rot)
+                    trans += np.array([0, move_speed, 0])
                 if c == "q":
-                    new_rot = old_rot - np.array([0, 0, move_speed])
-                    trans, rot = update_servos(old_trans, new_rot)
+                    rot -= np.array([0, 0, move_speed])
                 if c == "e":
-                    new_rot = old_rot + np.array([0, 0, move_speed])
-                    trans, rot = update_servos(old_trans, new_rot)
+                    rot += np.array([0, 0, move_speed])
                 if c == "r":
-                    new_trans = old_trans + np.array([0, 0, move_speed])
-                    trans, rot = update_servos(new_trans, old_rot)
+                    trans += np.array([0, 0, move_speed])
                 if c == "f":
-                    new_trans = old_trans - np.array([0, 0, move_speed])
-                    trans, rot = update_servos(new_trans, old_rot)
+                    trans -= np.array([0, 0, move_speed])
                 if c == "i":
-                    new_rot = old_rot - np.array([move_speed, 0, 0])
-                    trans, rot = update_servos(old_trans, new_rot)
+                    rot -= np.array([move_speed, 0, 0])
                 if c == "k":
-                    new_rot = old_rot + np.array([move_speed, 0, 0])
-                    trans, rot = update_servos(old_trans, new_rot)
+                    rot += np.array([move_speed, 0, 0])
                 if c == "j":
-                    new_rot = old_rot - np.array([0, move_speed, 0])
-                    trans, rot = update_servos(old_trans, new_rot)
+                    rot -= np.array([0, move_speed, 0])
                 if c == "l":
-                    new_rot = old_rot + np.array([0, move_speed, 0])
-                    trans, rot = update_servos(old_trans, new_rot)
+                    rot += np.array([0, move_speed, 0])
+                if c == " ":
+                    trans = np.array([0, 0, 0])
+                    rot = np.array([0, 0, 0])
+        try:
+            update_servos(trans, rot)
         except ValueError as ve:
            print(ve)
-           trans = old_trans
-           rot = old_rot
+
