bbeadce050e68df38adf743fc57c67ef5b1f2fc9
Amandus Krantz <krantz.amandus@protonmail.com>
2022-11-18 15:26:24 +0100

    read commands from serial connection, changed servo to arrays

--- ArduinoServoControllerFirmware/ArduinoServoControllerFirmware.ino
+++ ArduinoServoControllerFirmware/ArduinoServoControllerFirmware.ino
@@ -2,53 +2,64 @@
 
 enum commands {R, W};
 
-const int s1Pin = 1;
-const int s2Pin = 2;
-const int s3Pin = 3;
-const int s4Pin = 4;
-const int s5Pin = 5;
-const int s6Pin = 6;
-
 const int baudrate = 1000000;
 
-Servo s1;
-Servo s2;
-Servo s3;
-Servo s4;
-Servo s5;
-Servo s6;
+Servo servos[6];
+const int pins[6] = {1, 2, 3, 4, 5, 6};
+
+// splits a string at a separator
+// taken from https://stackoverflow.com/a/14824108/7362882
+String getValue(String data, char sep, int index) {
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == sep || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
 
 void setup() {
   //TODO:
   // - Would be great to do this dynamically
   // - - maybe require setting up the servos over serial?
 
-  s1.attach(s1Pin);
-  s2.attach(s2Pin);
-  s3.attach(s3Pin);
-  s4.attach(s4Pin);
-  s5.attach(s5Pin);
-  s6.attach(s6Pin);
+  for (i = 0; i < 6; i++) {
+    servos[i] = Servo;
+    servos[i].attach(pins[i]);
+  }
 
   Serial.begin(baudrate);
 }
 
 void loop() {
   if (Serial.available()) {
-    inByte = Serial.readBytesUntil('\n');
+    String data = Serial.readBytesUntil('\n');
 
-  // data packet
-  // <command> <servoId> [position] \n
+    // data packet
+    // <command> <servoId> [position] \n
 
-  switch (command) {
-    case W:
-      writeServoPosition();
-      break;
-    case R:
-      readServoPosition();
-      Serial.write();
-      break;
-  }
+    String command = getValue(data, ' ', 0);
+    int servoId = getValue(data, ' ', 1).toInt();
+
+    switch (command) {
+      case W:
+        int pos = getValue(data, ' ', 2).toInt();
+        writeServoPosition(servoId, pos);
+        break;
+      case R:
+        int pos = readServoPosition(servoId);
+        Serial.write(pos);
+        break;
+      default:
+        std::cout << "Unknown command" << std::endl;
+    }
   }
 }
 
@@ -58,7 +69,7 @@ int readServoPosition(int servoId) {
   return;
 }
 
-int writeServoPosition(int servoId) {
+int writeServoPosition(int servoId, int pos) {
   // writes new position to servo
 
   return;
