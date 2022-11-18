8fc0728f7399d2a321ec0d4a4316d187281c24cb
Amandus Krantz <krantz.amandus@protonmail.com>
2022-11-18 15:27:40 +0100

    added error handling for open serial connection

--- ArduinoServoControl.py
+++ ArduinoServoControl.py
@@ -6,4 +6,19 @@ class ArduinoServoControl:
                  baudrate=1000000,
                  deviceName="dev/tty."):
 
-        arduino = serial.Serial(deviceName, baudrate)
+        self.VERBOSE = verbose
+        try:
+            self.arduino = serial.Serial(deviceName, baudrate)
+        except SerialException as se:
+            print(se)
+        except ValueError as ve:
+            print(ve)
+        else:
+            if self.VERBOSE:
+                print("Succeeded to open port to device " + deviceName)
+
+    def __del__(self):
+        self.arduino.close()
+
+    def bulk_write(self, goal_positions):
+        self.arduino.write(goal_positions)
